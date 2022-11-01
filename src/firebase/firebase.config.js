744c76ca3ab9c4cf32372432f59f7b4c2878e1d5
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-11-01 22:49:58 +0600

    added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyAPbac5MFWFCg9Ry6GJpQJtNQwG0PYE7BE",
+  authDomain: "ginious-client-5f4c8.firebaseapp.com",
+  projectId: "ginious-client-5f4c8",
+  storageBucket: "ginious-client-5f4c8.appspot.com",
+  messagingSenderId: "689045158901",
+  appId: "1:689045158901:web:199052faf07cbdfd2dfebc",
+  measurementId: "G-8ZDEQ77R88"
 };
 
 // Initialize Firebase
